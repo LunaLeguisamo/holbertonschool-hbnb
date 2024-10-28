3bc4d4e8dff11616dc25dd199076825e3de7ae11
balemansteve <balemansteve@gmail.com>
2024-10-27 22:26:33 -0300

    Added whitespace between 'place_id:' and 'Place' / Changed error message in 'ValueError'

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -3,13 +3,13 @@ from app.models.place import Place
 from app.models.user import User
 
 class Review(BaseModel):
-    def __init__(self, text: str, rating: int, place_id:Place, user_id: User):
+    def __init__(self, text: str, rating: int, place_id: Place, user_id: User):
         super().__init__()
         self.text = text
         if rating >= 1 and rating <= 5:
             self.rating = rating
         else:
-            raise ValueError("Error")
+            raise ValueError("Rating must be between 1 and 5")
         self.place_id = place_id
         self.user_id = user_id
 
