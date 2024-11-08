608d9b88e8f7f5ebb07942c3371211831e4400d1
balemansteve <balemansteve@gmail.com>
2024-11-08 07:29:01 -0300

    test_users

--- part2/tests/test_users.py
+++ part2/tests/test_users.py
@@ -18,18 +18,33 @@ def test_user_creation_valid():
     assert response_data["first_name"] == "Lidia"
     assert response_data["last_name"] == "Butterley"
     assert response_data["email"] == "lbutterley0@bravesites.com"
-    
 
-def test_user_creation_invalid():
+
+def test_user_creation_empty_fields():
     '''
-    User creation test with invalid data.
+    User creation test with empty fields.
     '''
     data = {
         "first_name": "",
         "last_name": "",
-        "email": "lbutterley0"
+        "email": ""
     }
     response = requests.post(f"{BASE_URL}/users/", json=data)
     assert response.status_code == 400
     response_data = response.json()
     assert "error" in response_data
+
+def test_user_creation_invalid_email():
+    '''
+    User creation test with invalid email.
+    '''
+    data = {
+        "first_name": "Lidia",
+        "last_name": "Butterley",
+        "email": "lbutterley"
+    }
+    response = requests.post(f"{BASE_URL}/users/", json=data)
+    assert response.status_code == 400
+    response_data = response.json()
+    assert "error" in response_data
+    assert "email" in response_data
