df47cb3b1a53251cc6b22ecadd61436857fb1e32
Luna Leguisamo <9356@holbertonstudents.com>
2024-11-14 14:43:43 -0300

    task4

---
+++ part3/part2/app/models/amenity.py
@@ -0,0 +1,17 @@
+from . import BaseModel
+
+class Amenity(BaseModel):
+    def __init__(self, name: str):
+        super().__init__()
+        self._name = name
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, string):
+        if len(string) <= 50:
+            return self._name == string
+        else:
+            raise ValueError("Name is too long")
---
+++ part3/part2/app/models/review.py
@@ -0,0 +1,23 @@
+from . import BaseModel
+from app.models.place import Place
+from app.models.user import User
+
+class Review(BaseModel):
+    def __init__(self, text: str, rating: int, place_id: Place, user_id: User):
+        super().__init__()
+        self.text = text
+        self._rating = rating
+        self.place_id = place_id
+        self.user_id = user_id
+        
+    @property
+    def rating(self):
+        return self._rating
+    
+    @rating.setter
+    def rating(self, value):
+        if value >= 1 and value <= 5:
+            self.rating = value
+        else:
+            raise ValueError("Rating must be between 1 and 5")
+    
